b456b9f6a6eacd3069b234e2aece68b2367dd32c
Asger Lindberg <asgerlindberg@yahoo.com>
2022-02-28 10:03:45 +0100

    Salt missing from profile

--- SmoothNotesAPI/Models/Profile.cs
+++ SmoothNotesAPI/Models/Profile.cs
@@ -4,6 +4,7 @@ public class Profile
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string PW { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
     public string PrK { get; set; } = string.Empty;
     public string PuK { get; set; } = string.Empty;
     public DateTime CrDate { get; set; }
