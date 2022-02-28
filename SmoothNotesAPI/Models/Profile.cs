00ff03eeec909b81a2744594bc500effa4d13116
Asger Lindberg <asgerlindberg@yahoo.com>
2022-02-28 08:42:56 +0100

    Fixing Profile model

--- SmoothNotesAPI/Models/Profile.cs
+++ SmoothNotesAPI/Models/Profile.cs
@@ -2,6 +2,7 @@
 public class Profile
 {
     public Guid Id { get; set; }
+    public string Name { get; set; }
     public string PW { get; set; } = string.Empty;
     public string PrK { get; set; } = string.Empty;
     public string PuK { get; set; } = string.Empty;
